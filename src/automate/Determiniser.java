919846c7b34ab765249efdf564cbd634bdf64a7e
AdrienLH <adrien.uwindekwe@etu.univ-lehavre.fr>
2012-06-06 07:26:14 +0200

    Modif dans Determinisation

--- src/automate/Determiniser.java
+++ src/automate/Determiniser.java
@@ -8,10 +8,8 @@ import java.util.*;
 
 /**
  *
- * @author adrien
- * TPE Master 1
- * AS 2011-2012
- * Classe permettant d'effectuer la déterminisation d'un automate non deterministe
+ * @author adrien TPE Master 1 AS 2011-2012 Classe permettant d'effectuer la
+ * déterminisation d'un automate non deterministe
  */
 public class Determiniser {
 
@@ -21,7 +19,8 @@ public class Determiniser {
     }
 
     /*
-     * Méthode permettant de calculer le determiniser d'un automate quelconque passé en parametre
+     * Méthode permettant de calculer le determiniser d'un automate quelconque
+     * passé en parametre
      */
     public Automate determiniser(Automate automate) {
         State R = null;
@@ -38,8 +37,8 @@ public class Determiniser {
         } else {
             auStd = automate.getCopy();
         }
-        
-    
+
+
         String id_initial = auStd.getInitialStates().get(0).getId();
         T.addAll(auStd.getInitialStates());
         ArrayList<State> alStateP = new ArrayList<State>();
@@ -51,9 +50,9 @@ public class Determiniser {
             if (resultat.getState(P.getId()) == null) {
                 if (P.getId().equals(id_initial) && !P.isAcceptable()) {
                     resultat.addInitialState(P.getId());
-                } else if (P.getId().equals(id_initial) && P.isAcceptable()){
+                } else if (P.getId().equals(id_initial) && P.isAcceptable()) {
                     resultat.addInitialAcceptableState(P.getId());
-                }else{
+                } else {
                     resultat.addState(P.getId());
                 }
             }
@@ -63,50 +62,72 @@ public class Determiniser {
             }
 
             resultat.getState(P.getId()).addAttribute("visite", "visite");
-            HashSet<Character> listChar = new HashSet<Character>();
+            HashMap<Character, State> listChar = null;
 
             String id = "";
             boolean acceptable = false;
 
             for (Character a : auStd.getSigma()) {
+                listChar = new HashMap<Character, State>();
                 for (State st : alStateP) {
                     for (Transition t : auStd.getTransitions()) {
-                        if (t.getState1().getId().equals(st.getId()) && t.getLettre().equals(a) && !listChar.contains(a)) {
+                        if (t.getState1().getId().equals(st.getId()) && t.getLettre().equals(a) && listChar.containsKey(a)) {
+                            if (id.isEmpty() && listChar.get(a).getId().contains(t.getState2().getId())) {
+                                id = listChar.get(a).getId();
+                            } else if (id.isEmpty() && !listChar.get(a).getId().contains(t.getState2().getId())) {
+                                id = t.getState2().getId() + listChar.get(a).getId();
+                            } else if (!id.isEmpty() && listChar.get(a).getId().contains(t.getState2().getId())) {
+                                if (listChar.get(a).getId().contains(id)) {
+                                    id = listChar.get(a).getId();
+                                } else if (!listChar.get(a).getId().contains(id)) {
+                                    id = id + listChar.get(a).getId();
+                                }
+                            }
+                            if (listChar.get(a).isAcceptable()) {
+                                acceptable = true;
+                            }
+
+                        } else if (t.getState1().getId().equals(st.getId()) && t.getLettre().equals(a) && !listChar.containsKey(a)) {
                             if (id.isEmpty()) {
                                 id = t.getState2().getId();
-                            } else {
+                            } else if (!id.contains(t.getState2().getId())) {
                                 id = id + "," + t.getState2().getId();
                             }
+
                             if (t.getState2().isAcceptable()) {
                                 acceptable = true;
                             }
                         }
+
                     }
                     if (!id.isEmpty()) {
                         if (resultat.getState(id) == null) {
                             if (acceptable) {
                                 R = resultat.addAcceptableState(id);
+                                acceptable = false;
                             } else {
                                 R = resultat.addState(id);
                             }
+                            resultat.addTransition(P, R, a);
 
                         } else {
                             R = resultat.getState(id);
+                            resultat.addTransition(P, R, a);
                         }
-                        resultat.addTransition(P, R, a);
-                        acceptable = false;
-                        alStateVisite.add(P);
-                        listChar.add(a);
-                        if (!alStateVisite.contains(R)) {
-                            T.add(R);
-                        }
+                        listChar.put(a, R);
+
                         id = "";
                     }
 
+
                 }
-                
+                alStateVisite.add(P);
+                if (!alStateVisite.contains(R)) {
+                    T.add(R);
+                }
+
                 //============Gestion de l'etat puit ===============
-                if (listChar.size() != auStd.getSigma().size()) {
+                if (listChar.keySet().size() != auStd.getSigma().size()) {
                     if (resultat.getState("#") == null) {
                         resultat.addState("#");
                         for (Character c : auStd.getSigma()) {
@@ -114,11 +135,12 @@ public class Determiniser {
                         }
                     }
                     for (Character c : auStd.getSigma()) {
-                        if (!listChar.contains(a)) {
+                        if (!listChar.containsKey(a)) {
                             resultat.addTransition(P, resultat.getState("#"), a);
                         }
                     }
                 }
+                listChar.clear();
 
             }
             alStateP.clear();
